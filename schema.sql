40ddd38cb9faf9933cc5b9999933912a2287c1c1
Jerquanus Swann <jerquanus@gmail.com>
2021-01-23 18:07:27 -0500

    create department table

---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+
+/* Create database */
+CREATE DATABASE employeeTrackerDB;
+USE employeeTrackerDB;
+
+/* Create new table with a primary key that auto-increments, and a text field */
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- CREATE TABLE role (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   title VARCHAR(100) NOT NULL,
+--   salary
+--   PRIMARY KEY (id)
+-- );
